345c7e7b4476daa824f380f183fb8fb7bfa34fff
sami-sad-ik <sadiksami530@gmail.com>
2024-02-27 22:01:43 +0600

    final submit

--- assignment5.js
+++ assignment5.js
@@ -1,15 +1,24 @@
 let counter = 0;
 
 function addSeat() {
+  if (counter >= 8) {
+    return;
+  }
   counter += 1;
   document.getElementById("seat-count").innerText = counter;
 }
-let minusCounter = 40;
+let minusCounter = 8;
 function minusSeat() {
+  if (minusCounter <= 0) {
+    alert("All seats are booked!");
+    return;
+  }
   minusCounter--;
   document.getElementById("seat-minus").innerText = minusCounter;
 }
 const clr = document.getElementsByClassName("add-btn");
+let totalCost = 0;
+let grandTotalCost = 0;
 for (const btn of clr) {
   btn.addEventListener("click", function (event) {
     btn.classList.add("bg-[#1DD100]", "hover:bg-[#1DD100]");
@@ -26,5 +35,29 @@ for (const btn of clr) {
     tr.appendChild(td2);
     tr.appendChild(td3);
     list.appendChild(tr);
+    const total = document.getElementById("total");
+    totalCost = totalCost + 550;
+    total.innerText = totalCost;
+    const grandTotal = document.getElementById("grand-total");
+    grandTotal.innerText = totalCost;
+    return grandTotal;
   });
+}
+function couponApply() {
+  const couponCode = document.getElementById("input-field");
+  if (couponCode.value === "NEW15") {
+    grandTotalCost = totalCost - totalCost * 0.15;
+    document.getElementById("grand-total").innerText = grandTotalCost;
+  } else if (couponCode.value === "Couple20") {
+    grandTotalCost = totalCost - totalCost * 0.2;
+    document.getElementById("grand-total").innerText = grandTotalCost;
+  } else {
+    alert("Invalid coupon code");
+  }
+}
+function next() {
+  const hide = document.getElementById("hide");
+  hide.classList.add("hidden");
+  const display = document.getElementById("display");
+  display.classList.remove("hidden");
 }
